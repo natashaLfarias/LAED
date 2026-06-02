8912be03f2f46c5fdf281ac60452dc7f14f3b107
natashaLfarias <natasha.farias@aluno.ufca.edu.br>
2026-06-02 18:51:46 -0300

    Revert "Lista 03_AV1"

--- Criar_vetor.py
+++
@@ -1,8 +0,0 @@
-import random
-from random import randint
-def criar_vetor(n, lim_i, lim_f):
-  vetor = [0] * n
-  for i in range(n):
-      vetor[i] = random.randint(lim_i, lim_f)
-
-  return vetor
